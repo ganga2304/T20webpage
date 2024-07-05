d19e9d4c59d1f188264149287485ed03468e6835
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-05 11:18:03 +0530

    added sorting key file

---
+++ Script/sorts _keys.rb
@@ -0,0 +1,13 @@
+=begin
+Write a method that sorts keys in a hash by length of key and return
+the sorted hash.
+=end
+
+
+def sort_hash(hash)
+  return hash.sort_by { |key, _| key.to_s.length }.to_h
+end
+
+hash = { "ruby" => 1, "java" => 2, "python" => 3, "c" => 4 }
+sorted_hash = sort_hash(hash)
+puts sorted_hash 
