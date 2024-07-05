b97831652b089f9c0aa3453db45255d821b6b37f
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-05 12:34:24 +0530

    added codeStandards for the files examplemethods and maxNum

--- Script/maxNum.rb
+++ Script/maxNum.rb
@@ -1,4 +1,5 @@
 def max_number(num1, num2, num3, num4)
   max_value = num1 > num2 ? (num1 > num3 ? (num1 > num4 ? num1 : num4) : (num3 > num4 ? num3 : num4)) : (num2 > num3 ? (num2 > num4 ? num2 : num4) : (num3 > num4 ? num3 : num4))
-  result = 'This is the code to find the maximum value among four numbers. Make sure it follows all code standards and uses raw logic.'
+  result = 'This is the code to find the maximum value among four numbers. Make sure it follows all code '
 end
+
