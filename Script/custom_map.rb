fc95d445fd44d562c7c285ffd718ff7deb0b97ec
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-05 12:13:11 +0530

    custom_map file is added

--- Script/custom_map.rb
+++ Script/custom_map.rb
@@ -6,5 +6,5 @@ def custom_map(array)
   total
 end
 array = [1, 2, 3, 4, 5]
-doubled_array = custom_map(array) { |num| num-1}
-puts doubled_array
+new_array = custom_map(array) { |num| num-1}
+puts new_array
