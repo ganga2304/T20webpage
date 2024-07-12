7724fbc9c9e855e9358b857f3d37133af1cfa700
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-12 14:15:59 +0530

    updated with csv file

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,12 +1,12 @@
 class Match < ApplicationRecord
-    require 'csv'
-    has_many :players
- def self.Cricket
-  def initialize(path)
-    @path = path
-  end
-  def process_data
-    @data = CSV.read(@path, headers: true).map(&:to_h).map do |row|
+  require 'csv'
+  has_many :players
+  def self.Cricket
+    def initialize(path)
+      @path = path
+    end
+    def process_data
+      @data = CSV.read(@path, headers: true).map(&:to_h).map do |row|
       {
         'date' => row['date'],
         'location' => row['location'].to_i,
@@ -15,13 +15,13 @@ class Match < ApplicationRecord
         'score_team1' => row['score_team1'].to_i,
         'score_team2' => row['score_team2'].to_i
       }
-    end
+      end
     @data
   end
   def push_data
     @data = process_data
     @data.each do |player|
-      Match.create(date: player['date'], location: player['location'], team1_id: player['team1_id'], team2_id: player['team2_id'], score_team1: player['score_team1'], score_team2: player['score_team2'])
+      Match.create(date: players['date'], location: players['location'], team1_id: players['team1_id'], team2_id: players['team2_id'], score_team1: players['score_team1'], score_team2: players['score_team2'])
     end
   end
 end
